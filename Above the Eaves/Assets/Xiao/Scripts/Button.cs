471f21ca5d3b056668c767b98b7b6fbf52823298
KingproteaTheSanta <15942641949@163.com>
2026-04-29 13:53:37 +0800

    按钮

---
+++ Above the Eaves/Assets/Xiao/Scripts/Button.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [Header("���Tag")]
+    public string playerTag = "Player";          // ���Tag
+
+    [Header("�ƶ������Ŀ��")]
+    public Transform objectToMove;               // Ҫ�ƶ�������
+    public Transform targetPositionObject;       // �յ�����
+
+    [Header("�ƶ�����")]
+    public float moveSpeed = 5f;                 // �ƶ��ٶ�
+
+    private bool triggered = false;              // �Ƿ񴥷�
+
+    void Update()
+    {
+        if (triggered && objectToMove != null && targetPositionObject != null)
+        {
+
+            objectToMove.position = Vector3.MoveTowards(objectToMove.position, targetPositionObject.position, moveSpeed * Time.deltaTime);
+
+
+            if (Vector3.Distance(objectToMove.position, targetPositionObject.position) < 0.01f)
+            {
+                triggered = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!triggered && other.CompareTag(playerTag))
+        {
+            triggered = true;
+        }
+    }
+}
